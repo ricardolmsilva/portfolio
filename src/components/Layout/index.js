fc6ff9a0bcc98b333685ab8b74ebd0984456df68
Ricardo Silva <ricardolmsilva@me.com>
2020-05-11 13:13:42 +0100

    removing pwa

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,17 +1,15 @@
-import React, { useState } from "react"
+import React from "react"
 import { ThemeProvider } from "styled-components"
 
 import LightTheme from "../../themes/light"
 
 import GlobalStyles from "../../resources/styles/globalStyles"
 
-const Layout = ({ children }) => {
-  return (
-    <ThemeProvider theme={LightTheme}>
-      <GlobalStyles />
-      {children}
-    </ThemeProvider>
-  )
-}
+const Layout = ({ children }) => (
+  <ThemeProvider theme={LightTheme}>
+    <GlobalStyles />
+    {children}
+  </ThemeProvider>
+)
 
 export default Layout
